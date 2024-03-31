dab95b261f6570bdcdd67b07cad788910389618c
unknown <rickyyeung@sunnyvision.com>
2024-03-31 11:54:54 +0800

    createCampaign function added

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -15,22 +15,62 @@ contract MyContract {
         uint256[] donations;
         bool[] donationReleased;
     }
- 
-    mapping (uint256 => Campaign) public campaigns;
+
+    mapping(uint256 => Campaign) public campaigns;
 
     uint256 numberOfCampaigns = 0;
 
-    function addProofOfWork(uint256 _campaignId, string memory _newProofOfWork)
-    public {
+    event CampaignCreated(
+        uint256 indexed campaignId,
+        address owner,
+        string title,
+        uint256 target,
+        uint deadline
+    );
+
+    function createCampaign(
+        string memory _title,
+        string memory _description,
+        uint256 _target,
+        uint256 _deadline
+    ) public returns (uint256) {
+        require(_deadline > block.timestamp, "Deadline must be in the future");
+
+        Campaign storage campaign = campaigns[numberOfCampaigns];
+        campaign.owner = msg.sender;
+        campaign.title = _title;
+        campaign.description = _description;
+        campaign.target = _target;
+        campaign.deadline = _deadline;
+
+        emit CampaignCreated(
+            numberOfCampaigns,
+            msg.sender,
+            _title,
+            _target,
+            _deadline
+        );
+
+        numberOfCampaigns++;
+        return numberOfCampaigns - 1;
+    }
+
+    function addProofOfWork(
+        uint256 _campaignId,
+        string memory _newProofOfWork
+    ) public {
         Campaign storage campaign = campaigns[_campaignId];
-        require(msg.sender == campaign.owner, "Only the campaign owner can add proof of work");
+        require(
+            msg.sender == campaign.owner,
+            "Only the campaign owner can add proof of work"
+        );
 
         campaign.proofsOfWork.push(_newProofOfWork);
     }
 
-    function getProofsOfWork(uint256 _campaignId) public view returns (string[] memory) {
+    function getProofsOfWork(
+        uint256 _campaignId
+    ) public view returns (string[] memory) {
         return campaigns[_campaignId].proofsOfWork;
     }
-
- 
 }
