d61d4450008782d754a07a7a61b973709a79cafb
unknown <rickyyeung@sunnyvision.com>
2024-03-31 11:44:54 +0800

    added proofofwork function

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -9,65 +9,28 @@ contract MyContract {
         uint256 target;
         uint256 deadline;
         uint256 amountCollected;
+        string[] proofsOfWork;
         string image;
         address[] donators;
         uint256[] donations;
+        bool[] donationReleased;
     }
-
+ 
     mapping (uint256 => Campaign) public campaigns;
 
     uint256 numberOfCampaigns = 0;
 
-    function createCampaign ( address _owner, string memory _title , string memory  _description, uint256 _target, uint256 _deadline, string memory  _image ) public returns (uint256){
-        
-        Campaign storage campaign = campaigns[numberOfCampaigns];
+    function addProofOfWork(uint256 _campaignId, string memory _newProofOfWork)
+    public {
+        Campaign storage campaign = campaigns[_campaignId];
+        require(msg.sender == campaign.owner, "Only the campaign owner can add proof of work");
 
-        campaign.owner = _owner;
-        campaign.title = _title;
-        campaign.description = _description;
-        campaign.target = _target;
-        campaign.deadline = _deadline;
-        campaign.image = _image;
-
-        numberOfCampaigns++;
-
-        return numberOfCampaigns;
-
+        campaign.proofsOfWork.push(_newProofOfWork);
     }
 
-    function donateCampaign (uint256 _id) public payable {
-
-        uint256 amount = msg.value;
-
-        Campaign storage campaign = campaigns[_id];
-
-        campaign.donators.push(msg.sender);
-        campaign.donations.push(amount);
-
-        (bool sent,) = payable(campaign.owner).call{value:amount}("");
-
-        if (sent) {
-            campaign.amountCollected += amount;
-        }
-
+    function getProofsOfWork(uint256 _campaignId) public view returns (string[] memory) {
+        return campaigns[_campaignId].proofsOfWork;
     }
 
-    function getDonators(uint256 _id) view public returns (address[] memory, uint256[] memory) {
-        return (campaigns[_id].donators, campaigns[_id].donations);
-    }
-
-    function getCampaigns() public view returns ( Campaign[] memory) {
-
-        Campaign[] memory allCampaigns = new Campaign[](numberOfCampaigns);
-
-        for (uint i = 0; i < numberOfCampaigns; i++){
-
-            allCampaigns[i] = campaigns[i];
-
-        }
-
-        return allCampaigns;
-
-    }
  
 }
