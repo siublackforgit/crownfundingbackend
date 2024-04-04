9ee315bd9133fee140f233d708226db0a1da7b45
unknown <rickyyeung@sunnyvision.com>
2024-04-04 21:16:48 +0800

    removed unwant code

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,57 +1,27 @@
-require("@matterlabs/hardhat-zksync-solc");
 require("@nomiclabs/hardhat-ethers");
-/** @type import('hardhat/config').HardhatUserConfig */
+
 module.exports = {
-  zksolc: {
-    version: "1.3.9",
-    compilerSource: "binary",
+  solidity: {
+    version: "0.8.17",
     settings: {
       optimizer: {
         enabled: true,
+        runs: 200
       },
     },
   },
   networks: {
-    hardhat:{
-      
+    localhost: {
+      url: 'http://127.0.0.1:8545'
     },
-    zksync_testnet: {
-      url: "https://zksync2-testnet.zksync.dev",
-      ethNetwork: "goerli",
-      chainId: 280,
-      zksync: true,
-    },
-    zksync_mainnet: {
-      url: "https://zksync2-mainnet.zksync.io/",
-      ethNetwork: "mainnet",
-      chainId: 324,
-      zksync: true,
+    hardhat: {
+      // Configuration specific to the Hardhat network, if necessary.
     },
   },
   paths: {
     sources: "./contracts",
     tests: "./test",
-  },
-  solidity: {
-    version: "0.8.17",
-    defaultNetwork: 'sepolia',
-    paths: {
-      sources: "./contracts",  
-      cache: "./cache-zk",
-      artifacts: "./artifacts-zk"
-    },
-    networks: {
-       hardhat: {},
-       sepolia: {
-         url: 'https://rpc.ankr.com/eth_sepolia',
-         accounts: [`0x${process.env.PRIVATE_KEY}`]
-       }
-    },
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
-      },
-    },
+    cache: "./cache",
+    artifacts: "./artifacts"
   },
 };
